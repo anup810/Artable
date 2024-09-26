2c0033149f022e3256268f823cf7a991ccb22db7
Anup Saud <anupsaud10@gmail.com>
2024-09-25 23:09:36 -0400

    Renamed ViewController to AdminHomeVC for Admin functionality
    - Subclassed HomeVC as AdminHomeVC to customize the admin interface.
    - Disabled login button in AdminHomeVC to differentiate from user view.
    - Added 'Add Category' button to the navigation bar in AdminHomeVC.
    - Implemented initial layout for the admin screen.

---
+++ ArtableAdmin/AdminHomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Anup Saud on 2024-09-16.
+//
+
+import UIKit
+
+class AdminHomeVC: HomeVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Admin"
+        // Call your custom setup for the admin home
+        setupAdminUI()
+    }
+
+    func setupAdminUI() {
+        navigationItem.leftBarButtonItem?.isEnabled = false
+        let addCategoryButton = UIBarButtonItem(title: "Add Category", style: .plain, target: self, action: #selector(addCategory))
+        navigationItem.rightBarButtonItem = addCategoryButton
+    }
+    
+    @objc func addCategory() {
+        // Transition to AddCategoryVC
+
+    }
+}
