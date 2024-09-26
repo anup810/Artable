8eb45bb078e3b54dab762822e8160f5735c3cf7b
Anup Saud <anupsaud10@gmail.com>
2024-09-25 21:49:08 -0400

    Created Product Details screen: - Added UI elements for displaying product title, description, image, and price. - Integrated Kingfisher for loading product images asynchronously. - Implemented currency formatting for the product price. - Designed layout with 'Add to Cart' and 'Keep Shopping' buttons. - Added a tap gesture to dismiss the product detail view. - Included basic product data binding and visual effects (UIVisualEffectView) for enhanced presentation.

--- Artable/Controller/ProductVC.swift
+++ Artable/Controller/ProductVC.swift
@@ -70,6 +70,14 @@ extension ProductVC : UITableViewDelegate, UITableViewDataSource{
         }
         return UITableViewCell()
     }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = ProductDetailsVC()
+        let selectedProduct = products[indexPath.row]
+        vc.product = selectedProduct
+        vc.modalTransitionStyle = .crossDissolve
+        vc.modalPresentationStyle = .overCurrentContext
+        present(vc, animated: true, completion: nil)
+    }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 200
     }
